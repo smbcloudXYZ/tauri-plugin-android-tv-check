75f2d995eadf94fdc85cc2c86aab1edf0af51a1d
keypair34 <work@setoelkahfi.se>
2025-08-18 10:45:07 +0200

    put back the dashes

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,7 +1,7 @@
 import { invoke } from "@tauri-apps/api/core";
 
 export async function check(): Promise<string | null> {
-  return await invoke<{ value?: string }>("plugin:androidtvcheck|check").then(
+  return await invoke<{ value?: string }>("plugin:android-tv-check|check").then(
     (r) => (r.value ? r.value : null),
   );
 }
